2b42262ce06edc209661e591caa8aa8b7a994e06
tomershalom40 <tomer.shalom.40.talpiot@gmail.com>
2019-03-16 11:55:36 +0200

    Added 1 meter ahead function getObjects still doesnt recognize

---
+++ app/src/main/java/com/google/ar/core/examples/java/helloar/Main.java
@@ -0,0 +1,61 @@
+package com.google.ar.core.examples.java.helloar;
+import com.google.ar.core.examples.java.helloar.TwoDLine;
+import com.google.ar.core.examples.java.helloar.HelloArActivity;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Camera;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.Point;
+import com.google.ar.core.Point.OrientationMode;
+import com.google.ar.core.PointCloud;
+import com.google.ar.core.Pose;
+import com.google.ar.core.Session;
+import com.google.ar.core.Trackable;
+import com.google.ar.core.TrackingState;
+import com.google.ar.core.examples.java.common.helpers.CameraPermissionHelper;
+import com.google.ar.core.examples.java.common.helpers.DisplayRotationHelper;
+import com.google.ar.core.examples.java.common.helpers.FullScreenHelper;
+import com.google.ar.core.examples.java.common.helpers.SnackbarHelper;
+import com.google.ar.core.examples.java.common.helpers.TapHelper;
+import com.google.ar.core.examples.java.common.rendering.BackgroundRenderer;
+import com.google.ar.core.examples.java.common.rendering.ObjectRenderer;
+import com.google.ar.core.examples.java.common.rendering.ObjectRenderer.BlendMode;
+import com.google.ar.core.examples.java.common.rendering.PlaneRenderer;
+import com.google.ar.core.examples.java.common.rendering.PointCloudRenderer;
+import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+
+import java.io.IOException;
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class Main {
+    /**
+    float [] polygon = {2.1422215, 2.2984343, -2.0325158, 2.3001137, -1.3123358, 2.2148664,
+            1.5647601, 1.5942166, 1.9798915, 1.4904157, 2.019597, 1.4638214, 2.195204,
+            1.3388413, 2.2772322, -0.7356939, 2.1780584, -2.1052012, 2.0436008, -2.3001137,
+            -0.64699745, -2.3001137, -1.055216, -1.4534655, -2.2772322, 1.3810529, -2.2495296,
+            ,1.8210052};
+    ArrayList<ArrayList<float[]>> objects;
+    Plane floor;
+    */
+}
